f4e931267086549537eed5c6fdbd94c6c652227d
Nikkinorin <zhazgulbakytbekova@gmail.com>
2025-05-28 07:40:24 +0500

    Update bot_webhook.py

--- bot_webhook.py
+++ bot_webhook.py
@@ -1,35 +1,24 @@
-from flask import Flask, request
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 import os
 
 TOKEN = '8166412144:AAH6gFmQPOjGn3CSoDmwJuSBzSxEfbQ8x8M'
+PORT = int(os.environ.get('PORT', 5000))
 
-app = Flask(__name__)
-application = ApplicationBuilder().token(TOKEN).build()
-
-# Проверочный маршрут для корневого адреса
-@app.route('/', methods=['GET'])
-def home():
-    return 'Сервер Flask работает!'
-
-# Обработчик команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает через Webhook!")
 
-# Обработчик всех текстовых сообщений
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
 
-application.add_handler(CommandHandler("start", start))
-application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-@app.route(f'/{TOKEN}', methods=['POST'])
-async def webhook():
-    update = Update.de_json(request.get_json(force=True), application.bot)
-    await application.process_update(update)
-    return 'ok'
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        url_path=TOKEN,
+        webhook_url=f"https://bot-calc-with-command-button.onrender.com/{TOKEN}"
+    )
